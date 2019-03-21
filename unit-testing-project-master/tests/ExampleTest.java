22f942f9c1eea29436a89050961dad76692a2fd1
Jim-Keys <jkeys2@villanova.edu>
2019-03-21 00:09:29 -0400

    All but Hashcode in Item
    Needed to swap out the code I was working on in my IntelliJ and what was on Github because it copied instead of moving.

--- unit-testing-project-master/tests/ExampleTest.java
+++ unit-testing-project-master/tests/ExampleTest.java
@@ -1,8 +1,8 @@
-
+package csc4700;
 import org.junit.Test;
+import csc4700.Backup;
 
 import static org.junit.Assert.*;
-
 
 // Please delete this file prior to submitting your project.
 
@@ -12,6 +12,64 @@ public class ExampleTest {
     @Test
     public void testJUnitIsConfiguredCorrectly() {
         assertTrue(true);
+    }
+
+
+    //Item Tests
+    @Test
+    public void testDescription() {
+        Item testItem = new Item();
+        testItem.setDescription("Test");
+        assertEquals("Test", testItem.getDescription());
+    }
+    @Test
+    public void testCost() {
+        Item testItem = new Item();
+        testItem.setCost(1);
+        assertEquals(1, testItem.getCost());
+    }
+    @Test
+    public void testName() {
+        Item testItem = new Item();
+        testItem.setName("A");
+        assertEquals("A", testItem.getName());
+    }
+    @Test
+    public void testEqualsTrue() {
+        Item itemA = new Item();
+        itemA.setCost(1); itemA.setDescription("item"); itemA.setName("Test Item");
+        Item itemB = itemA;
+        assertEquals(true, itemA.equals(itemB));
+    }
+    @Test
+    public void testEqualsFalse() {
+        Item itemA = new Item();
+        Item itemB = new Item();
+        itemA.setCost(1); itemA.setDescription("item"); itemA.setName("Test Item");
+        itemB.setCost(2); itemB.setDescription("itemB"); itemB.setName("Test Item 2");
+        assertEquals(false, itemA.equals(itemB));
+    }
+    @Test
+    public void testEqualsFalse2() {
+        Item itemA = new Item();
+        int itemB = 4;
+        itemA.setCost(1); itemA.setDescription("item"); itemA.setName("Test Item");
+        assertEquals(false, itemA.equals(itemB));
+    }
+    @Test
+    public void testHashCode() {
+        //Someone else is gonna have to write this test because I don't know hashcode
+    }
+    //End of Item Tests
+
+    @Test
+    public void testSerializeShoppingCartNull() {
+        Backup b = new Backup();
+        try {
+            b.serializeShoppingCart(null);
+        } catch (NullPointerException e) {
+            assertTrue(true);
+        }
     }
 
 }
